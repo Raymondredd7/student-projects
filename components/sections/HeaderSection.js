a7fcdc87fda5f3d56345b89146148c8279385587
Raymondredd7 <raymondreddington843@gmail.com>
2023-02-16 23:31:51 +0100

    changes

--- components/sections/HeaderSection.js
+++ components/sections/HeaderSection.js
@@ -10,11 +10,11 @@ export default function HeaderSection() {
     <div className="h-full flex flex-col justify-center items-center">
       <h1 className="text-white	text-5xl">Projektet e nxenesve te shkolles SHMTK</h1>
       <Link href="/projects">
-      <button class="mt-6 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Shiko projektet
-  </span>
-</button>
+      <button type="button"
+            class="mt-6 inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+            data-mdb-ripple="true" data-mdb-ripple-color="light">
+            Shiko Projektet
+          </button>
       </Link>
     </div>
   </div>
--- pages/projects.js
+++ pages/projects.js
@@ -9,44 +9,52 @@ export default function projects() {
   <div className='h-full flex flex-col justify-center items-center pt-6'>
     <h1 className='text-5xl'>Projektet</h1>
   </div>
-  <div className='grid grid-cols-3'>
-    <div>
-      <h1>Massum</h1>
-      <p>Our school tradıtıonally takes part ın the Educational Rendezvous wıtch ıs organızed by...</p>
-      <Link href="/projects">
-      <button class="mt-6 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Lexo me shume
-  </span>
-</button>
-      </Link>
-    <Image src="/images/massum.jpg"width={500} height={500}/>
-    </div>
-    <div>
-      <h1>Massum</h1>
-      <p>Our school tradıtıonally takes part ın the Educational Rendezvous wıtch ıs organızed by...</p>
-      <Link href="/projects">
-      <button class="mt-6 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Lexo me shume
-  </span>
-</button>
-      </Link>
-    <Image src="/images/massum.jpg" width={500} height={500}/>
-    </div>
-    <div>
-      <h1>Massum</h1>
-      <p>Our school tradıtıonally takes part ın the Educational Rendezvous wıtch ıs organızed by...</p>
-      <Link href="/projects">
-      <button class="mt-6 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Lexo me shume
-  </span>
-</button>
-      </Link>
-    <Image src="/images/massum.jpg" width={500} height={500}/>
+  <div className='mt-12 grid grid-cols-3'>
+  <div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">MASSUM</h5>
+      <p class="text-gray-700 text-base mb-4">
+      Our school tradıtıonally takes part ın the Educational Rendezvous wıtch ıs organızed by  he Youth Association of Technical High Schools in Macedonia MASSUM.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Lexo me shume</button>
     </div>
   </div>
+</div>
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">NANSEN</h5>
+      <p class="text-gray-700 text-base mb-4">
+      In the school year 2017/2018 professors from our school are taking part in Nansen model for integrated education (NMIE).
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Lexo me shume</button>
+    </div>
+  </div>
+</div>
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!">
+      <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">ENGLISH CLUB</h5>
+      <p class="text-gray-700 text-base mb-4">
+      Every Monday after school classes our students have activities in the English club.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Lexo me shume</button>
+    </div>
+  </div>
+</div>
+  </div>
+
+  
     </Layout>
   )
 }
