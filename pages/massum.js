bd383a1b705c9f921faf22eb5d34cdbf33fdc949
Raymondredd7 <raymondreddington843@gmail.com>
2023-02-20 00:34:32 +0100

    new shit

---
+++ pages/massum.js
@@ -0,0 +1,10 @@
+import Massum from '@/components/sections/Massum'
+import Layout from '@/components/layout/Layout'
+
+export default function massum() {
+  return (
+    <Layout>
+        <Massum />
+    </Layout>
+  )
+}
